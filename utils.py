609ffb12edc92b53a6696e7c3b2f0b2f344c9053
wooyybb <102472575+wooyybb@users.noreply.github.com>
2023-05-24 11:49:09 +0900

    utils.py

--- utils.py
+++ utils.py
@@ -91,82 +91,98 @@ class utils:
 
 	def make_query_ML_classificaiton(self, data_path, selected_model,  label_col, class_cnt, nan_check_flag, useless_features, numeric_col, categorical_col,
 		scaler_flag, class_imbalance_flag):
+
 		Query = f'I want to make python code about machine learning classification {selected_model} model following the steps I described.\n'
-		Query = f'And, I don\'t want code annotations.\n'
-		Query += f'In the code, you need to ignore warnings by using the "warinings" library.\n'
+		Query += f'In the code, you need to ignore warnings by using the "warinings" library, and I want to plus these library\n'
+		Query += f'Please import LabelEncoder, MinMaxScaler, train_test_split, log_loss, f1_score, optuna, TPESampler on the code.\n'
+
 		Query += f'Then, you need to use pandas library to load data from {data_path}.\n'
 		Query += f'In data, The label to be predicted is {label_col}, and the label consists of {class_cnt} types.\n'
 		Query += f'Also it has numeric columns such as {numeric_col}, and categoical columns such as {categorical_col}.\n'
 
 		if useless_features is not False:
-			Query += f'Since data has useless features, please drop {useless_features} columns.\n'#useless features drop
+			Query += f'Since data has useless features, please drop {useless_features} columns.\n'
+		
+		if nan_check_flag is not False:
+			Query += f'Since data has nan value, please fill nan data to zero and remove columns if each column has more than 20% missing values.' 
+
+		if scaler_flag is not False:
+			Query += f'Since scaler_flag is not false, please scale data using scikit-learn MinMaxScaler.\n'
+
+		if class_imbalance_flag is not False:
+			Query += f'Since there is a current class imbalance problem, use imblearn SMOTE library to oversample only the training data.\n'
 
 		Query += f'First, encode categorical columns using scikit-learn labelencoder, but if there is no categorical variable, ignore this.\n'
 		Query += f'Next, encode the label data (y of train or y of validation) with labelencoder.\n'
-		
-		if nan_check_flag is not False:
-			Query += 'Since data has nan value, please fill nan data to zero and remove columns if each column has more than 20% missing values.' #fill nan
-		
+		Query += f'Then, split data into X and y.\n'
 
-		if scaler_flag is not False:
-			Query += 'Since scaler_flag is not false, please scale data using scikit-learn MinMaxScaler.\n'
-
-		if class_imbalance_flag is not False:
-			Query += 'Since there is a current class imbalance problem, use imblearn SMOTE library to oversample only the training data.\n'
+		Query += f'Also, separate 10 percent of the validation data using the train_test_split function, and set the stratify parameter of the function to True.\n' 
+		Query += f'In these code, set all SEED values (and random_state parameters) to 42. \n'
 
 		if selected_model == "RandomForest":
-			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_rf} and write the objective function of optuna. Do not use pruner and use the TPESampler.\n'
+			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_rf} and write the objective function of optuna.\n'
+			Query += f'Do not use pruner and use the TPESampler.\n'
 		elif selected_model == "ExtraTree":
-			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_ex} and write the objective function of optuna. Do not use pruner and use the TPESampler.\n'
+			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_ex} and write the objective function of optuna.\n'
+			Query += f'Do not use pruner and use the TPESampler.\n'
 		elif selected_model == "XGBoost":
 			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_xgb} and write the objective function of optuna.\n'
-			Query+= f'In the fit part of the internal of objective function, set eval_set to validation data and earlystopping round to 100. Do not use pruner and use the TPESampler.\n'
+			Query += f'Do not use pruner and use the TPESampler.\n'
 		elif selected_model == "LightGBM":
-			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_lgbm} and write the objective function of optuna. Do not use pruner and use the TPESampler.\n'
+			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_lgbm} and write the objective function of optuna.\n'
+			Query += f'Do not use pruner and use the TPESampler.\n'
 
-		Query += f'After these steps, separate 10 percent of the validation data using the train_test_split function, and set the stratify parameter of the function to True.\n' 
-		Query += f'Also, set all SEED values (and random_state parameters) to 42. \n'
-  
-		Query += 'Set the evaluation metric inside the objective function to macro f1-score, but fit part metric is logloss.\n'
-		Query += 'Last, check you have properly imported the necessary libraries.'
+		Query += f'Continue with these steps, set the evaluation metric inside the objective function to macro f1-score, but fit part metric is logloss.\n'
+		Query += f'After these steps, print best hyperparameters and score,valid f1 score and valid mse\n'
+		
+		Query += f'Finally, check your code, so that I can use code avaliable at once'
+
 		return Query
 
 	def make_query_ML_regression(self, data_path, selected_model, label_col, nan_check_flag, useless_features, numeric_col, categorical_col, scaler_flag):
 		Query = f'I want to make python code about machine learning regression {selected_model} model following the steps I described.\n'
-		Query = f'And, I don\'t want code annotations.\n'
 		Query += f'In the code, you need to ignore warnings by using the "warinings" library.\n'
+
 		Query += f'Then, you need to use pandas library to load data from {data_path}.\n'
 		Query += f'In data, The label to be predicted is {label_col}.\n'
 		Query += f'Also it has numeric columns such as {numeric_col}, and categoical columns such as {categorical_col}.\n'
 
 		if useless_features is not False:
-			Query += f'Since data has useless features, please drop {useless_features} columns.\n'#useless features drop	
+			Query += f'Since data has useless features, please drop {useless_features} columns.\n'#useless features drop
+		
+		if nan_check_flag is not False:
+			Query += f'Since data has nan value, please fill nan data to zero and remove columns if each column has more than 20% missing values.' #fill nan
+
+		if scaler_flag is not False:
+			Query += f'Since scaler_flag is not false, please scale data using scikit-learn MinMaxScaler.\n'
+
 
 		Query += f'First, encode categorical columns using scikit-learn labelencoder, but if there is no categorical variable, ignore this.\n'
 		Query += f'Next, encode the label data (y of train or y of validation) with labelencoder.\n'
-		
-		if nan_check_flag is not False:
-			Query += 'Since data has nan value, please fill nan data to zero and remove columns if each column has more than 20% missing values.' #fill nan
-		
-		if scaler_flag is not False:
-			Query += 'Since scaler_flag is not false, please scale data using scikit-learn MinMaxScaler.\n'
+		Query += f'Then, split data into X and y.\n'
+
+		Query += f'Also, separate 10 percent of the validation data using the train_test_split function, and set the stratify parameter of the function to True.\n' 
+		Query += f'In these code, set all SEED values (and random_state parameters) to 42. \n'
+
 
 		if selected_model == "RandomForest":
-			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_rf} and write the objective function of optuna. Do not use pruner and use the TPESampler.\n'
+			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_rf} and write the objective function of optuna.\n'
+			Query += f'Do not use pruner and use the TPESampler.\n'
 		elif selected_model == "ExtraTree":
-			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_ex} and write the objective function of optuna. Do not use pruner and use the TPESampler.\n'
+			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_ex} and write the objective function of optuna.\n'
+			Query += f'Do not use pruner and use the TPESampler.\n'
 		elif selected_model == "XGBoost":
-			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_xgb} and write the objective function of optuna. Do not use pruner and use the TPESampler.\n'
-			Query+= f'In the fit part of the internal of objective function, set eval_set to validation data and earlystopping round to 100. Do not use pruner and use the TPESampler.\n'
+			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_xgb} and write the objective function of optuna.\n'
+			Query += f'Do not use pruner and use the TPESampler.\n'
 		elif selected_model == "LightGBM":
-			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_lgbm} and write the objective function of optuna. Do not use pruner and use the TPESampler.\n'
-		Query += f'After these steps, separate 10 percent of the validation data using the train_test_split function, and set the stratify parameter of the function to True.\n'
+			Query += f'Please optimize hyperparameters using optuna library, and refer to this dictionary {self.param_lgbm} and write the objective function of optuna.\n'
+			Query += f'Do not use pruner and use the TPESampler.\n'
 
-		Query += f'Also, set all SEED values (and random_state parameters) to 42. \n'
-  
-		Query += 'Set the evaluation metric inside the objective function to Mean Square Error, but fit part metric is l2.\n'
+		Query += f'Continue with these steps, set the evaluation metric inside the objective function to Mean Square Error, but fit part metric is l2.\n'
+		Query += f'After these steps, print best hyperparameters and score,valid f1 score and valid mse.\n'
+		
+		Query += f'Finally, check your code, so that I can use code avaliable at once'
 
-		Query += 'Last, check you have properly imported the necessary libraries.'
 		return Query
 
 	def classification_process(self,path_param, useless_param, label_col_param):
@@ -230,5 +246,3 @@ class utils:
 		for model in self.selected_model:
 			Query.append(self.make_query_ML_regression(data_path, model, label_col, nan_check_flag, useless_features, numeric_col, categorical_col, scaler_flag))
 		return Query
-
-
